65265a0a307ee326369cac90d8cf24414c9d40ea
Steven Clontz <steven.clontz@gmail.com>
2017-06-16 12:42:10 -0500

    method for calculating max satisfactory attempts

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,3 +2,12 @@ class Course < ApplicationRecord
   has_many :students
   has_many :standards, through: :standard_category
   has_many :standard_categories
+
+  def maximum_satsifactory_attempts
+    standard_categories.inject(0) do |sum,standard_category|
+      sum +
+      standard_category.standards.length *
+      standard_category.satsifactory_limit
+    end
+  end
+end
