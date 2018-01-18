2762419070fb4aaedf98770e5bd3c890c21c6ec6
Steven Clontz <steven.clontz@gmail.com>
2018-01-18 16:18:55 -0600

    randomly assign exercises to students when creating assessments

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,4 +2,30 @@ class Assessment < ApplicationRecord
   has_many :covered_standards, dependent: :destroy
   has_many :standards, through: :covered_standards
   belongs_to :course
+
+  after_create :assign_exercises_to_students
+
+  private
+
+  def assign_exercises_to_students
+    covered_standards.each do |covered_standard|
+      exercises = Exercise.where(
+        standard_id: covered_standard.standard_id
+      ).order("RANDOM()").limit(4)
+      if exercises.length > 0
+        course_student_ids = course.student_ids
+        student_ids_slices = course_student_ids
+          .shuffle
+          .each_slice(course_student_ids.length / exercises.length)
+          .to_a
+        exercises.each_with_index do |exercise,index|
+          ExerciseVersion.create(
+            exercise_id: exercise.id,
+            covered_standard_id: covered_standard.id,
+            student_ids: student_ids_slices[index]
+          )
+        end
+      end
+    end
+  end
 end
