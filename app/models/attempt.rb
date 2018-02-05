fcba9ea8c316f99b35ba5cd803b3b04a1abf7aa3
Steven Clontz <steven.clontz@gmail.com>
2018-02-05 10:42:31 -0600

    more appropriate failed provisional symbol

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -10,7 +10,7 @@ class Attempt < ApplicationRecord
     "unsatisfactory" => "✘",
     "incomplete" => "⚠",
     "provisional" => "✱",
-    "provisional_unsatisfactory" => "✱✘",
+    "provisional_unsatisfactory" => "✱⚠",
     "provisional_satisfactory" => "✱✔",
     "satisfactory" => "✔",
     "unknown" => "?"
