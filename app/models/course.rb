8f4e941a83d02aced2a63a305e4e4a9acdf38a34
Steven Clontz <steven.clontz@gmail.com>
2018-03-08 15:14:16 -0600

    add report on course for date method

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,7 @@ class Course < ApplicationRecord
   has_many :standard_categories
   has_many :grade_specifications
   has_many :attempt_categories
+  has_many :attempts, through: :students
 
   def maximum_satisfactory_attempts
     standard_categories.inject(0) do |sum,standard_category|
@@ -15,5 +16,16 @@ class Course < ApplicationRecord
 
   def students_by_name
     students.reorder(:last_name,:first_name)
+  end
+
+  def report_attempts_on_date date_string
+    all_attempts = attempts.where("DATE(attempted_on) = ?", date_string)
+    satisfactory_count = all_attempts.where(mark: "satisfactory").count
+    provisional_count = all_attempts.where(mark:[
+       "provisional",
+       "provisional_unsatisfactory",
+       "provisional_satisfactory"
+      ]).count
+    "#{all_attempts.count} attempts, #{satisfactory_count} ✔s, #{provisional_count} ✱s"
   end
 end
