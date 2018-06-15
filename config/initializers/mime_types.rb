4d796e119509f6b3a907185242010b7dc095058e
Steven Clontz <steven.clontz@gmail.com>
2018-06-15 12:45:29 -0500

    fix tex content type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
+Mime::Type.register "text/tex", :tex
+
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
