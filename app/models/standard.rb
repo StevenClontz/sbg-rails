b5d5f972568581153d27c7ad6997dd76feed5b38
Steven Clontz <steven.clontz@gmail.com>
2019-01-02 16:41:16 -0600

    change standard exercise import to work from mastr

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -26,33 +26,36 @@ class Standard < ApplicationRecord
     successes.to_f / students.length
   end
 
-  def create_exercises_from_file filepath
-    source = File.readlines filepath
+  def create_exercises_from_mastr filepath
+    seeds = (20..99).to_a.sample(20)
     exercises = []
-    state = :waiting
-    source.each do |line|
-      case state
-      when :waiting
-        if line.start_with? '\begin{problem}'
-          state = :problem
-          exercises << Exercise.new(standard_id: id, description: '', solution: '')
-        elsif line.start_with? '\begin{solution}'
-          state = :solution
-        end
-      when :problem
-        if line.start_with? '\end{problem}'
-          state = :waiting
-        else
-          exercises[-1].description << line
-        end
-      when :solution
-        if line.start_with? '\end{solution}'
-          state = :waiting
-        else
-          exercises[-1].solution << line
+    seeds.each do |seed|
+      source = File.readlines(filepath + "/0" + seed.to_s + ".tex")
+      state = :waiting
+      source.each do |line|
+        case state
+        when :waiting
+          if line.strip.start_with? '\begin{exerciseStatement}'
+            state = :statement
+            exercises << Exercise.new(standard_id: id, description: '', solution: '')
+          elsif line.strip.start_with? '\begin{exerciseAnswer}'
+            state = :answer
+          end
+        when :statement
+          if line.strip.start_with? '\end{exerciseStatement}'
+            state = :waiting
+          else
+            exercises[-1].description << line
+          end
+        when :answer
+          if line.strip.start_with? '\end{exerciseAnswer}'
+            state = :waiting
+          else
+            exercises[-1].solution << line
+          end
         end
       end
+      exercises.each(&:save)
     end
-    exercises.each(&:save)
   end
 end
