bf0e6c8f739c36ee21b5a67b71b90e697c672625
Steven Clontz <steven.clontz@gmail.com>
2020-04-23 07:57:17 +0000

    increase timeout buffer

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,7 +4,7 @@ class Section < ApplicationRecord
 
   def sync_progress_to_gd
     students.each do |s|
-      sleep 5.seconds # to avoid timeouts
+      sleep 10.seconds # to avoid timeouts
       s.create_gd_sheet
       s.save_gd_sheet
     end
